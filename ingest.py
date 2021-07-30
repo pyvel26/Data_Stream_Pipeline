34cc32073d4f0bfee25ae115393ddaf390db7478
Estervelle Bennett <bennettestervelle@gmail.com>
2021-07-30 15:56:10 -0400

    Add files via upload

---
+++ ingest.py
@@ -0,0 +1,25 @@
+import requests
+import psycopg2
+from getpass import getpass 
+
+with requests.get("http://127.0.0.1:5000/very_large_request/100000000", stream=True) as r:
+
+    conn = psycopg2.connect(dbname="test_stream",
+                            user="postgres",
+                            password = "LonelyGuy77")
+    cur = conn.cursor()
+    sql = "INSERT INTO transactions (txid, uid, amount) VALUES (%s, %s, %s)"
+
+    buffer = ""
+    for chunk in r.iter_content(chunk_size=1):
+        if chunk.endswith(b'\n'):
+            t = eval(buffer)
+            print(t)
+            cur.execute(sql, (t[0], t[1], t[2]))
+            conn.commit()
+            buffer = ""
+        else:
+            buffer += chunk.decode()
+
+
+               
